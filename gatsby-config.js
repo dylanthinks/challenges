016546f0e9934bafe9ae3df6023d12423ae78082
Dylan <hotlinedfive@Hotlines-iMac-4.local>
2021-01-10 10:20:23 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  siteMetadata: {
+    title: "challenges",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "6aIauqPU9pXp9il836v8V78LRU4U_va6AM14RNNUCF0",
+        spaceId: "",
+      },
+    },
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
